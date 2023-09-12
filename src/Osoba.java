0ad05c5a6c4f1db3bd39643a7d2b486b9195297b
QveczekDev <rdud1471@gmail.com>
2023-09-12 11:11:19 +0200

    programowanie obiektowe

---
+++ src/Osoba.java
@@ -0,0 +1,29 @@
+public class Osoba {
+private String imie;
+   private String nazwisko;
+   private int wiek;
+
+   public String getImie() {
+      return imie;
+   }
+
+   public void setImie(String imie) {
+      this.imie = imie;
+   }
+
+   public String getNazwisko() {
+      return nazwisko;
+   }
+
+   public void setNazwisko(String nazwisko) {
+      this.nazwisko = nazwisko;
+   }
+
+   public int getWiek() {
+      return wiek;
+   }
+
+   public void setWiek(int wiek) {
+      this.wiek = wiek;
+   }
+}
